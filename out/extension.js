8785ecc6dbf307d7f7e62963037dbeb8db5d7a4f
Arvin Lee <angevill20006cn@gmail.com>
2018-06-22 18:18:55 +0800

    add keybinding to extension

--- out/extension.js
+++ out/extension.js
@@ -13,12 +13,26 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand
+    (
+        'extension.sayHello', 
+        function () 
+        {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World! again~');
-    });
+            vscode.window.showInformationMessage('Hello World! again~');
+        }
+    );
+
+    let dance = vscode.commands.registerCommand
+    (
+        'extension.sayHey', 
+        function()
+        {
+            vscode.window.showInformationMessage('Hey Silkroad');
+        }
+    );
 
     context.subscriptions.push(disposable);
 }
