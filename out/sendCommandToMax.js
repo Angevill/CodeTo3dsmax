8172a588f61d34679915940a43c5263b9daad2f5
angevill <angevill20006cn@gamil.com>
2018-06-25 01:46:25 +0800

    overload SendMessageW

--- out/sendCommandToMax.js
+++ out/sendCommandToMax.js
@@ -4,22 +4,28 @@
 // you should rebuild like "node-gyp rebuild --target=v1.7.12 --disturl="https://atom.io/download/electron" "
 // otherwise , you will get tons of error~
 const ffi = require('ffi');
-const ref = require('ref');
 
 // ffi requires the name of a dll to call methods from, along with an object
 // describing the methods we want access to - their names and an array which
 // contains their return type and an array of input types
+
 var winapi = new ffi.Library("User32", {
     "FindWindowA": ["int32", ["string", "string"]],
     "FindWindowExA": ["int32", ["int32", "int32", "string", "string"]],
     //TODO: i dont konw how to make lParam valid under this arg list....
-    "SendMessageW": ["int64", ["int32", "uint32", "uint32", "string"]],
+    "SendMessageW": ["int64", ["int32", "uint32", "uint32", ffi.types.CString]],
     "SetWindowTextW": ["bool", ["int32", "string"]]
+});
+
+// try to "overloading" SendMessageW with different arg type
+var winapiINT = new ffi.Library("User32", {
+    "SendMessageW" : ["int64", ["int32", "uint32", "uint32", ffi.types.int32]]
 });
 
 const WM_SETTEXT = 0x000C;
 const WM_CHAR = 0x0102;
 const VK_RETURN = 0x0D;
+
 
 function TEXT(text){
     return new Buffer(text, 'ucs2').toString('binary');
@@ -38,10 +44,8 @@ exports.sendPrompt = function(cmd) {
     
     console.log("child Script ID", scirptEditor);
 
-    // win32.SendMessageW(scirptEditor, WM_SETTEXT, 0, cmd);
-    // win32.SendMessageW(scirptEditor, WM_CHAR, VK_RETURN, 0);
     winapi.SendMessageW(scirptEditor, WM_SETTEXT, 0, TEXT(cmd));
-    winapi.SendMessageW(scirptEditor, WM_CHAR, VK_RETURN, String(0));// kind of buggy with String(int)...
+    winapiINT.SendMessageW(scirptEditor, WM_CHAR, VK_RETURN, 1);// kind of buggy with String(int)...
     console.log("child Script Inject Done");
     scirptEditor = null;
 }
